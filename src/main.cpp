d2a735cba27e31f32fb6d9bee70e424e0552c0dc
FRANCOIS RUBIO <37674416-fran31r@users.noreply.replit.com>
2025-01-24 10:08:39 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  const float coutHoraire = 4.0;
+  int velosDispos = 20;
+  int capacite= 20;
+  int choix;
+
+  cout << "******* Gestion d'une station Vélô ********" <<endl <<endl;
+  cout << "------------------------------------------- "<<endl <<endl;
+  cout << "Vélos disponibles : " <<velosDispos <<endl;
+    cout << "Point d'attache disponibles : "<<velosDispos-capacite <<endl;
+    cout << "------------------------------------------- "<<endl <<endl;
+     cout << "1.Emprunter un vélô électrique"<<endl;
+      cout << "2. Restituer un vélô électrique"<<endl;
+     cout << "3. Estimer le coût de la location"<<endl;
+      cout << "4. Quitter l'application"<<endl;
+    return 0;
+}
